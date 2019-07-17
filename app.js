dc897e0657e92c82cf00de9f459fd63112594ec8
jacs <wbruce0110@gmail.com>
2019-07-17 15:43:05 +0800

    setup express app

---
+++ app.js
@@ -0,0 +1,33 @@
+// include packages and setup server
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const session = require('express-session')
+const passport = require('passport')
+
+const port = 3000
+
+// setup the app
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+app.use(methodOverride('_method'))
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.use(session({
+  secret: 'akpitdx',
+  resave: 'false',
+  saveUninitialized: 'false'
+}))
+
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.get('/', (req, res) => {
+  res.send('Hello world')
+})
+
+app.listen(port, () => {
+  console.log('Listening on express app...')
+})
